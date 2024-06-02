e5ed3cc7a484241219e680bd5fdd533fb21a1dbc
Manu <info@gigeirt.ch>
2024-06-02 22:10:45 +0200

    wip

--- tests/Feature/Resources/Contacts/Contacts/Requests/DeleteContactRequestTest.php
+++ tests/Feature/Resources/Contacts/Contacts/Requests/DeleteContactRequestTest.php
@@ -3,14 +3,12 @@
 namespace Bexio\Resources\Contacts\Contacts\Requests;
 
 use Bexio\Resources\Contacts\Contacts\Contact;
-use Bexio\Resources\Contacts\Contacts\Enums\ContactType;
 use function Pest\Faker\fake;
 
 it('can create a contact', function () {
-    $contact = new Contact(name_1: fake()->company());
-    $request = new CreateContactRequest($contact);
+    $request = new CreateContactRequest(new Contact(name_1: fake()->company()));
     $response = testClient()->send($request);
-    $contact = Contact::from($response->json());
+    $contact = $request->createDtoFromResponse($response);
 
     $request = new DeleteContactRequest($contact->id);
 
