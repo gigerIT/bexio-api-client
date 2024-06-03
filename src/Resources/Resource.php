57f0646685fb0130856bd6571e75aa82cc51599a
Manu <info@gigeirt.ch>
2024-06-03 10:01:02 +0200

    fix: resources from

--- src/Resources/Resource.php
+++ src/Resources/Resource.php
@@ -8,51 +8,47 @@ class Resource
 {
     public static function from(array $data): static
     {
-        $reflection = new \ReflectionClass(static::class);
-        $constructor = $reflection->getConstructor();
-        $parameters = $constructor ? $constructor->getParameters() : [];
-        $args = [];
+        $reflector = new \ReflectionClass(static::class);
+        $instance = $reflector->newInstanceWithoutConstructor();
 
-        foreach ($parameters as $parameter) {
-            $name = $parameter->getName();
-            $type = $parameter->getType() && !$parameter->getType()->isBuiltin()
-                ? new \ReflectionClass($parameter->getType()->getName())
-                : null;
+        foreach ($data as $key => $value) {
+            if ($reflector->hasProperty($key)) {
+                $property = $reflector->getProperty($key);
+                $propertyType = $property->getType();
 
-            if ($type && $type->isEnum()) {
-                $args[$name] = isset($data[$name]) ? $type->getMethod('from')->invoke(null, $data[$name]) : null;
-            } else {
-                $args[$name] = $data[$name] ?? null;
+                if ($propertyType instanceof \ReflectionNamedType && enum_exists($propertyType->getName())) {
+                    $enumClass = $propertyType->getName();
+                    $property->setValue($instance, $enumClass::from($value));
+                } else {
+                    $property->setValue($instance, $value);
+                }
             }
         }
 
-        $instance = $reflection->newInstanceArgs($args);
-
-        // Set properties that are not declared in the constructor
-        $properties = $reflection->getProperties();
-        foreach ($properties as $property) {
-            $name = $property->getName();
-            if (!array_key_exists($name, $args) && array_key_exists($name, $data)) {
-                $property->setAccessible(true);
-                $type = $property->getType() && !$property->getType()->isBuiltin()
-                    ? new \ReflectionClass($property->getType()->getName())
-                    : null;
-
-                if ($type && $type->isEnum()) {
-                    $property->setValue($instance, isset($data[$name]) ? $type->getMethod('from')->invoke(null, $data[$name]) : null);
+        // Call the constructor manually if needed
+        $constructor = $reflector->getConstructor();
+        if ($constructor) {
+            $constructorParams = [];
+            foreach ($constructor->getParameters() as $param) {
+                $paramName = $param->getName();
+                if (array_key_exists($paramName, $data)) {
+                    $constructorParams[] = $data[$paramName];
                 } else {
-                    $property->setValue($instance, $data[$name]);
+                    $constructorParams[] = $param->isDefaultValueAvailable() ? $param->getDefaultValue() : null;
                 }
             }
+            $reflector->getConstructor()->invokeArgs($instance, $constructorParams);
         }
 
         return $instance;
     }
 
+
     public static function collect(array $resources): array
     {
         return array_map(fn($resource) => self::from($resource), $resources);
     }
+
 
     public function toArray(): array
     {
