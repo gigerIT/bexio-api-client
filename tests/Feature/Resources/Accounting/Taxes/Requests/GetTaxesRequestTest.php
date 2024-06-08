966fa6f37454922fc292018acfc2eb31ffc2f2cc
Manu <info@gigeirt.ch>
2024-06-08 21:06:04 +0200

    switch to laravel-data standalone package

--- tests/Feature/Resources/Accounting/Taxes/Requests/GetTaxesRequestTest.php
+++ tests/Feature/Resources/Accounting/Taxes/Requests/GetTaxesRequestTest.php
@@ -7,8 +7,6 @@ use Bexio\Resources\Accounting\Taxes\Tax;
 it('can get Taxes', function () {
     $taxes = Tax::useClient(testClient())->all();
 
-    dump($taxes[0]);
-
     expect($taxes)->toBeArray()
         ->and($taxes[0])->toBeInstanceOf(Tax::class)
         ->and($taxes[0]->id)->toBeInt();
