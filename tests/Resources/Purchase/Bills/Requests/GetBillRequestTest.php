2f66959784be883742d3678512b1cb0a5ebab19f
Manu <info@gigeirt.ch>
2024-06-09 10:32:06 +0200

    bill tests

--- tests/Resources/Purchase/Bills/Requests/GetBillRequestTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Bexio\Resources\Purchase\Bills\Bill;
-
-it('can get a Bill', function () {
-    $bill = Bill::useClient(testClient())->find('0933d1a1-0632-4c82-a315-e3918539633a');
-    expect($bill)->toBeInstanceOf(Bill::class);
-});
