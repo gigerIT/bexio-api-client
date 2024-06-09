b606c0482d0d192e7404b9e8dcbcf1cdd7b8724d
Manu <info@gigeirt.ch>
2024-06-09 10:14:03 +0200

    wip

--- src/Resources/Sales/MwstType.php
+++ src/Resources/Sales/MwstType.php
@@ -4,7 +4,7 @@ namespace Bexio\Resources\Sales;
 
 enum MwstType: int
 {
-    case INCLUDING = 1;
-    case EXCLUDING = 2;
-    case EXEMPT = 3;
+    case INCLUDING = 0;
+    case EXCLUDING = 1;
+    case EXEMPT = 2;
 }
--- tests/Resources/Sales/Invoices/Requests/IssueInvoiceRequestTest.php
+++ tests/Resources/Sales/Invoices/Requests/IssueInvoiceRequestTest.php
@@ -3,6 +3,8 @@
 use Bexio\Resources\Sales\Invoices\Invoice;
 
 it('can issue an invoice', function () {
+
+
     $response = Invoice::useClient(testClient())->issue(1);
 
     expect($response->successful())->toBeTrue();
