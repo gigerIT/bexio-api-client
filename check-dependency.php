664d18e335f58c0f4dcc55e8f22fc6cd7af05dd6
Manu <info@gigeirt.ch>
2024-06-11 22:16:40 +0200

    fix: check-dependency

--- check-dependency.php
+++ check-dependency.php
@@ -1,20 +1,42 @@
 <?php
 
-
 $packageName = 'laravel/framework';
 $optionalPackage = 'gigerit/spatie-laravel-data-standalone';
 
-// Check if the package is installed
-$installed = false;
-exec("composer show -N", $output);
-foreach ($output as $line) {
-    if (strpos($line, $packageName) !== false) {
-        $installed = true;
-        break;
+// Function to check if a package is installed
+function isPackageInstalled($packageName)
+{
+    exec("composer show -N", $output, $returnVar);
+    if ($returnVar !== 0) {
+        echo "Error: Unable to execute 'composer show'.\n";
+        exit(1);
     }
+    foreach ($output as $line) {
+        if (strpos($line, $packageName) !== false) {
+            return true;
+        }
+    }
+    return false;
 }
 
-// If the optional package is not installed and the main package is not installed, add the optional package
-if (!$installed) {
-    exec("composer require $optionalPackage");
+// Check if the main package is installed
+$mainPackageInstalled = isPackageInstalled($packageName);
+
+// Check if the optional package is installed
+$optionalPackageInstalled = isPackageInstalled($optionalPackage);
+
+// If the main package is not installed and the optional package is not installed, add the optional package
+if (!$mainPackageInstalled && !$optionalPackageInstalled) {
+    echo "$packageName is not installed. Installing $optionalPackage...\n";
+    exec("composer require $optionalPackage", $requireOutput, $requireReturnVar);
+    if ($requireReturnVar !== 0) {
+        echo "Error: Unable to require $optionalPackage.\n";
+        exit(1);
+    } else {
+        echo "$optionalPackage has been successfully installed.\n";
+    }
+} elseif ($mainPackageInstalled) {
+    echo "$packageName is already installed.\n";
+} elseif ($optionalPackageInstalled) {
+    echo "$optionalPackage is already installed.\n";
 }
