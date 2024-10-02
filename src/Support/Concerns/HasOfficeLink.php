d2620399d851df0ece4d64fb4ffa7748cad41dbb
Manu <info@gigeirt.ch>
2024-10-02 15:49:19 +0200

    fix: office link comments

--- src/Support/Concerns/HasOfficeLink.php
+++ src/Support/Concerns/HasOfficeLink.php
@@ -7,11 +7,17 @@ use Bexio\Resources\Resource;
 /** @mixin Resource */
 trait HasOfficeLink
 {
+    /**
+     * get the bexio deeplink of the current resource
+     */
     public function officeLink(): string
     {
         return self::OFFICE_BASE_URI . str_replace('{id}', (string)$this->id, self::SHOW_URI);
     }
 
+    /**
+     * get the bexio deeplink of the resource by id
+     */
     public static function officeLinkFor(int $id): string
     {
         return self::OFFICE_BASE_URI . str_replace('{id}', (string)$id, self::SHOW_URI);
