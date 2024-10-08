715ff9f2fd5d252f741646dcd0feb74659a9b2a7
Manu <info@gigeirt.ch>
2024-10-08 10:37:07 +0200

    fea: add accounting/currency with default id's

---
+++ src/Resources/Accounting/Currencies/Currency.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+
+namespace Bexio\Resources\Accounting\Currencies;
+
+use Bexio\Resources\Resource;
+
+class Currency extends Resource
+{
+    //Default ID's for currencies in Bexio (most users never change this list)
+    const DEFAULT_ID = [
+        'CHF' => 1,
+        'EUR' => 2,
+        'USD' => 3,
+        'GBP' => 4,
+        'BRL' => 5,
+        'JPY' => 6,
+        'CNY' => 7,
+    ];
+
+}
