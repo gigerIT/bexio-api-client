e5ab284b792afbe85e641068100f45425acf4084
Manu <mg@jgid.com>
2025-10-16 08:59:58 +0200

    feat: add HasOfficeLink trait to Contact class and define SHOW_URL constant

--- src/Resources/Contacts/Contacts/Contact.php
+++ src/Resources/Contacts/Contacts/Contact.php
@@ -11,6 +11,7 @@ use Bexio\Resources\Contacts\Contacts\Requests\GetContactsRequest;
 use Bexio\Resources\Contacts\Contacts\Requests\SearchContactRequest;
 use Bexio\Resources\Contacts\Contacts\Requests\UpdateContactRequest;
 use Bexio\Resources\Resource;
+use Bexio\Support\Concerns\HasOfficeLink;
 use Bexio\Support\Data\SearchCriteria;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
@@ -18,11 +19,15 @@ use RuntimeException;
 
 class Contact extends Resource
 {
+    use HasOfficeLink;
+
     const INDEX_REQUEST = GetContactsRequest::class;
     const SHOW_REQUEST = GetContactRequest::class;
     const CREATE_REQUEST = CreateContactRequest::class;
     const UPDATE_REQUEST = UpdateContactRequest::class;
     const DELETE_REQUEST = DeleteContactRequest::class;
+
+    public const SHOW_URL = '/index.php/kontakt/show/id/{id}';
 
     public ?string $updated_at;
     public ?string $profile_image;
@@ -32,37 +37,36 @@ class Contact extends Resource
 
     public function __construct(
         public int|ContactType $contact_type_id = ContactType::COMPANY,
-        public ?int            $id = null,
-        public ?string         $nr = null,
-        public ?string         $name_1 = null,
-        public ?string         $name_2 = null,
-        public ?int            $salutation_id = null,
-        public ?string         $salutation_form = null,
-        public ?int            $titel_id = null,
-        public ?string         $birthday = null,
-        public ?string         $address = null,
-        public ?string         $postcode = null,
-        public ?string         $city = null,
-        public ?int            $country_id = null,
-        public ?string         $mail = null,
-        public ?string         $mail_second = null,
-        public ?string         $phone_fixed = null,
-        public ?string         $phone_fixed_second = null,
-        public ?string         $phone_mobile = null,
-        public ?string         $fax = null,
-        public ?string         $url = null,
-        public ?string         $skype_name = null,
-        public ?string         $remarks = null,
-        public ?int            $language_id = null,
+        public ?int $id = null,
+        public ?string $nr = null,
+        public ?string $name_1 = null,
+        public ?string $name_2 = null,
+        public ?int $salutation_id = null,
+        public ?string $salutation_form = null,
+        public ?int $titel_id = null,
+        public ?string $birthday = null,
+        public ?string $address = null,
+        public ?string $postcode = null,
+        public ?string $city = null,
+        public ?int $country_id = null,
+        public ?string $mail = null,
+        public ?string $mail_second = null,
+        public ?string $phone_fixed = null,
+        public ?string $phone_fixed_second = null,
+        public ?string $phone_mobile = null,
+        public ?string $fax = null,
+        public ?string $url = null,
+        public ?string $skype_name = null,
+        public ?string $remarks = null,
+        public ?int $language_id = null,
 
 
-        public ?string         $contact_group_ids = null,
-        public ?string         $contact_branch_ids = null,
-        public int             $user_id = 1,
-        public int             $owner_id = 1,
+        public ?string $contact_group_ids = null,
+        public ?string $contact_branch_ids = null,
+        public int $user_id = 1,
+        public int $owner_id = 1,
 
-    )
-    {
+    ) {
     }
 
 
